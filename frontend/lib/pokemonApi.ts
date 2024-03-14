45851c38a80df94c28c23f7cdbd193fa9be16367
candraburhanudin15 <candraburhanudin33@gmail.com>
2024-03-14 10:37:33 +0700

    fix pokemonapi.ts

--- frontend/lib/pokemonApi.ts
+++ frontend/lib/pokemonApi.ts
@@ -1,32 +1,3 @@
-// const POKEMON_API = "https://pokecanov.shamirhusein.my.id/pokemon/poke";
-// export async function getPokemonList() {
-//     try {
-//         const response = await fetch(POKEMON_API);
-//         if (!response.ok) {
-//             throw new Error('Failed to fetch Pokemon list');
-//         }
-//         const data = await response.json();
-//         return data.results;
-//     } catch (error) {
-//         console.error('Error fetching Pokemon list:', error);
-//         return [];
-//     }
-// }
-
-// export async function getPokemon(name: string) {
-//     try {
-//         const response = await fetch(POKEMON_API + "/" + name);
-//         if (!response.ok) {
-//             throw new Error('Failed to fetch Pokemon data');
-//         }
-//         const data = await response.json();
-//         return data;
-//     } catch (error) {
-//         console.error('Error fetching Pokemon data:', error);
-//         return null;
-//     }
-// }
-
 const POKEMON_API = "https://pokecanov.shamirhusein.my.id/pokemon/poke";
 
 export async function getPokemonList() {
