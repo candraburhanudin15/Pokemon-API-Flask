94f627fd03ed3f2cb707e18dcc369e1190277a7d
candraburhanudin15 <candraburhanudin33@gmail.com>
2024-03-14 08:15:12 +0700

    add cors in flask

--- backend/run_backup.py
+++ backend/run_backup.py
@@ -2,6 +2,8 @@ import os
 from os.path import join, dirname
 from app import init_app
 from dotenv import load_dotenv
+from flask import Flask
+from flask_cors import CORS
 
 dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
@@ -11,6 +13,7 @@ config = "config.Config"
 # init_app('name', config)
 # name in here can be changes to project name.
 app = init_app("app", config)
+CORS(app)
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
--- frontend/next.dockerfile
+++ frontend/next.dockerfile
@@ -11,7 +11,7 @@ RUN npm run build --verbose
 
 EXPOSE 3000
 
-# CMD ["npm", "start"]
-CMD ["npm", "run", "dev"]
+CMD ["npm", "start"]
+# CMD ["npm", "run", "dev"]
 
  
